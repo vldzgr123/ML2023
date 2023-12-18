b138c319f97f03462ec146b142da698b0e92157e
vldzgr123 <vlad.z.00@inbox.ru>
2023-12-18 15:45:28 +0600

    first web done

---
+++ ML_RGR/web_1.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import streamlit as st
+from PIL import Image
+
+#Заголовок
+st.title("Информация об авторе")
+
+st.write('Загребельный Владислав Александрович, ФИТ-221.')
+st.write(
+    "Тема: «Разработка Web-приложения (дашборда) для инференса (вывода) моделей ML и анализа данных»"
+)
+
+#Загрузка фотографии
+st.image(
+    Image.open("ML_RGR\Pictures\web_1_pictures\photo_2023-07-30_15-17-12.jpg"),
+    width=450,
+)
