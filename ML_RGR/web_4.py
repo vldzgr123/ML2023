2dd08d0b34a814d52ca3014d33f3de4690b6a05d
vldzgr123 <vlad.z.00@inbox.ru>
2023-12-28 19:18:25 +0600

    rgr no done

--- ML_RGR/web_4.py
+++ ML_RGR/web_4.py
@@ -1,4 +1,28 @@
 import streamlit as st
-from ML_RGR.Functions import buildModel
+from Functions.selectModelFunc import * 
+import pandas as pd
+import numpy as np
+import pickle
 
+uploaded_file = st.file_uploader("Выберите файл датасет", type="csv")
 
+data = pd.read_csv(uploaded_file)
+
+if data is not None:
+    
+    st.title("Предсказание поставлена ли бомба")
+
+    type_model_select = st.selectbox(
+        "Выберите тип модели",
+        ["Обучение с учителем", "Обучение без учителя", "Ансабль", "Нейронная сеть"],
+    )
+
+    if type_model_select is not None:
+        selected_model = ModelTypeSelector(type_model_select)
+
+    data = FeaturesSelect()
+
+    button_start = st.button("Предсказать")
+
+    if button_start:
+        ModelPrediction(selected_model, data)
