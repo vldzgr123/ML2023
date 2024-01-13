223d76e2302be2cfc77646dc14133369e86ae2d5
Vladisalv Zagrebelniy <vlszgr@mail.ru>
2024-01-13 12:47:05 +0600

    rgr done

--- ML_RGR/Functions/selectModelFunc.py
+++ ML_RGR/Functions/selectModelFunc.py
@@ -42,56 +42,64 @@ def ModelTypeSelector(model_type):
 def FeaturesSelect():
     st.write("time_left - длительность раунда")
 
-    time_left = st.number_input("time_left:", value=175.0)
+    time_left = st.number_input(
+        "time_left:", value=0.010000, min_value=0.010000, max_value=175.000000
+    )
 
     st.write("ct_score - количество очков у команды контр-террористов")
 
-    ct_score = st.number_input("ct_score:", value=0)
+    ct_score = st.number_input("ct_score:", value=0, min_value=0, max_value=32)
 
     st.write("t_score - количество очков у террористов")
 
-    t_score = st.number_input("t_score:", value=0)
+    t_score = st.number_input("t_score:", value=0, min_value=0, max_value=33)
 
     st.write(
         "ct_health и t_health - суммарное здоровье контр-террористов и террористов соответственно"
     )
 
-    ct_health = st.number_input("ct_health:", value=500)
-    t_health = st.number_input("t_health:", value=500)
+    ct_health = st.number_input("ct_health:", value=0, min_value=0, max_value=500)
+    t_health = st.number_input("t_health:", value=0, min_value=0, max_value=500)
 
     st.write(
         "ct_armor и t_armor - суммарная броня контр-террористов и террористов соответственно"
     )
 
-    ct_armor = st.number_input("ct_armor:", value=0)
-    t_armor = st.number_input("t_armor:", value=0)
+    ct_armor = st.number_input("ct_armor:", value=0, min_value=0, max_value=500)
+    t_armor = st.number_input("t_armor:", value=0, min_value=0, max_value=500)
 
     st.write(
         "ct_money и t_money - суммарное количество денег контр-террористов и террористов соответственно"
     )
 
-    ct_money = st.number_input("ct_money:", value=4000)
-    t_money = st.number_input("t_money:", value=4000)
+    ct_money = st.number_input("ct_money:", value=0, min_value=0, max_value=80000)
+    t_money = st.number_input("t_money:", value=0, min_value=0, max_value=80000)
 
     st.write(
         "ct_helmets и t_helmets - суммарное количество шлемов контр-террористов и террористов соответственно"
     )
 
-    ct_helmets = st.number_input("ct_helmets:", value=0)
-    t_helmets = st.number_input("t_helmets:", value=0)
+    ct_helmets = st.number_input("ct_helmets:", value=0, min_value=0, max_value=5)
+    t_helmets = st.number_input("t_helmets:", value=0, min_value=0, max_value=5)
 
     st.write(
         "ct_defuse_kits - количество наборов обезвреживания бомбы у контр-террористов"
     )
 
-    ct_defuse_kits = st.number_input("ct_defuse_kits:", value=0)
+    ct_defuse_kits = st.number_input(
+        "ct_defuse_kits:", value=0, min_value=0, max_value=5
+    )
 
     st.write(
         "ct_players_alive и t_players_alive - количество живых контр-террористов и террористов соответственно"
     )
 
-    ct_players_alive = st.number_input("ct_players_alive:", value=5)
-    t_players_alive = st.number_input("t_players_alive:", value=5)
+    ct_players_alive = st.number_input(
+        "ct_players_alive:", min_value=0, max_value=5, value=0
+    )
+    t_players_alive = st.number_input(
+        "t_players_alive:", min_value=0, max_value=5, value=0
+    )
 
     data = pd.DataFrame(
         {
@@ -144,7 +152,7 @@ def ModelPrediction(model, data):
         case "NN":
             model_loaded = keras.models.load_model("ML_RGR/Models/NN.h5")
             NN_pred = np.around(model_loaded.predict(data)[0][0])
-            st.write(NN_pred)
+            st.write(int(NN_pred))
         case "Bagging":
             with open("ML_RGR\Models\Bagging.pickle", "rb") as file:
                 Bagging = pickle.load(file)
