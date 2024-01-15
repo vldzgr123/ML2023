95be898bf380b37b08c0fea9bbb1136ceaf4ad85
Vladisalv Zagrebelniy <vlszgr@mail.ru>
2024-01-15 12:13:02 +0600

    8Labdone

--- ML_RGR/web_1.py
+++ ML_RGR/web_1.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 
-#Заголовок
+
 st.title("Информация об авторе")
 
 st.write('Загребельный Владислав Александрович, ФИТ-221.')
@@ -9,7 +9,7 @@ st.write(
     "Тема: «Разработка Web-приложения (дашборда) для инференса (вывода) моделей ML и анализа данных»"
 )
 
-#Загрузка фотографии
+
 st.image(
     Image.open("ML_RGR/Pictures/web_1_pictures/photo_2023-07-30_15-17-12.jpg"),
     width=450,
